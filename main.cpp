e1c0edad58b5150d7d453566163f25d42ac779a7
yooseungyeon31 <ysy8317433@gmail.com>
2026-03-12 09:34:38 +0900

    중복 안되게 수정

--- main.cpp
+++ main.cpp
@@ -2,32 +2,76 @@
 
 using namespace std;
 //45개의 공 중 6개를 뽑는 프로그램 만들기
+//-----------------------------------------------
 
+//void Random()
+//{
+//	srand((unsigned int)time(NULL));
+//
+//
+//	cout << "랜덤으로 6개를 뽑습니다" << endl;
+//
+//	for (int i = 0; i < 6; i++)
+//	{
+//		int SelectBall = rand() % 45;
+//		cout << SelectBall << " ";
+//
+//	}
+//	cout << endl;
+//}
+//
+// 
+//
+//int main()
+//{
+//	int Ball[45] = { 0, };//공의 크기
+//	int SelectBall;
+//
+//	Random();
+//
+//	return 0;
+//}
 
-void Random()
-{
-	srand((unsigned int)time(NULL));
-
-
-	cout << "랜덤으로 6개를 뽑습니다" << endl;
-
-	for (int i = 0; i < 6; i++)
-	{
-		int SelectBall = rand() % 45;
-		cout << SelectBall << " ";
-
-	}
-	cout << endl;
-}
-
-
+//--------------------------------------------------
+//포인터 heap으로
 
 int main()
 {
-	int Ball[45] = { 0, };//공의 크기
-	int SelectBall;
+	int Size = 45;
 
-	Random();
+	int* Ball = new int[Size];
+
+	
+
+	srand((unsigned int)time(NULL));
+
+	cout << "랜덤으로 6개를 뽑습니다" << endl;
+
+
+	for (int i = 0; i < 6; i++)
+	{
+		Ball[i] = (rand() % 45)+1;
+
+		for (int j = 0; j < i; j++)
+		{
+			if (Ball[i] == Ball[j])
+			{
+				i--;
+				break;
+			}
+		}
+
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		cout << Ball[i] << " ";
+	}
+	cout << endl;
+
+	delete[] Ball;
+	Ball = nullptr;
+
 
 	return 0;
 }
